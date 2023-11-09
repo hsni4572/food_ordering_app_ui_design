9fbc28f3eeda092992070edaf6c3cf546294df2f
Husnain Ahmed <150255176+hsni4572@users.noreply.github.com>
2023-11-09 08:07:08 -0800

    // none

---
+++ lib/itempage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class itempage extends StatelessWidget {
+  const itempage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
